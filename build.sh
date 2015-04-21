2eac5e24a58a4fe904fc79624b6a7eb65439aed6
Edward Tjörnhammar <ed@cflags.cc>
2015-04-21 12:11:26 +0200

    Use proguard to minimize the distzip

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-mvn clean install:install-file dependency:unpack install assembly:single
+mvn clean install:install-file dependency:unpack install assembly:single && mvn proguard:proguard
