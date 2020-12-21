dc5682001d1fee6b76f351794ddb90dbf0423501
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2020-12-21 17:54:25 +0100

    Replace usage of deprecated method IconLoader.getIcon(String)

--- src/main/java/org/nixos/idea/icon/NixIcons.java
+++ src/main/java/org/nixos/idea/icon/NixIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class NixIcons {
-    public static final Icon FILE = IconLoader.getIcon("/org/nixos/idea/icon/nixos-icon.png");
+    public static final Icon FILE = IconLoader.getIcon("/org/nixos/idea/icon/nixos-icon.png", NixIcons.class);
 }
