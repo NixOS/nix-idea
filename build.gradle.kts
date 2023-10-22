f41ccf6fc12efa9897d16659afbfb14768b28d6c
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2023-10-22 12:49:00 +0200

    Update gradle-intellij-plugin from 1.13.3 to 1.16.0
    (Fixes build failure after updating Gradle in the last commit)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     // Java support
     id("java")
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij") version "1.16.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "2.1.0"
     // grammarkit - read more: https://github.com/JetBrains/gradle-grammar-kit-plugin
