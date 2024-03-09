28a36ae51cc009d8d263e87c03f78c342a4adad8
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2024-03-09 00:29:44 +0000

    Use precompiled script plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,11 @@ import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.tasks.RunPluginVerifierTask
 
 plugins {
-    // Java support
     id("java")
-    // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.17.2"
-    // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "2.2.0"
-    // grammarkit - read more: https://github.com/JetBrains/gradle-grammar-kit-plugin
-    id("org.jetbrains.grammarkit") version "2022.3.2.2"
+    id("org.jetbrains.intellij")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.grammarkit")
+    id("local.bump-version")
 }
 
 // Import variables from gradle.properties file
@@ -206,5 +203,3 @@ tasks {
     }
 
 }
-
-apply(from = "gradle/bumpVersion.gradle.kts")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+pluginManagement {
+    includeBuild("gradle/plugins")
+    plugins {
+        // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+        id("org.jetbrains.intellij") version "1.17.2"
+        // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+        id("org.jetbrains.changelog") version "2.2.0"
+        // grammarkit - read more: https://github.com/JetBrains/gradle-grammar-kit-plugin
+        id("org.jetbrains.grammarkit") version "2022.3.2.2"
+    }
+}
+
 rootProject.name = "NixIDEA"
