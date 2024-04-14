7dc749a488637fe1bca6a24c0c1b04f52c26a31c
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2024-04-14 13:30:46 +0200

    Remove unnecessary qualification in resource directory
    Examples and plugins of JetBrains don't use fully qualified
    sub-directories. So, I assume it is not necessary.

--- src/main/java/org/nixos/idea/icon/NixIcons.java
+++ src/main/java/org/nixos/idea/icon/NixIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class NixIcons {
-    public static final Icon FILE = IconLoader.getIcon("/org/nixos/idea/icon/nixos-icon.png", NixIcons.class);
+    public static final Icon FILE = IconLoader.getIcon("/icons/nixos-icon.png", NixIcons.class);
 }
